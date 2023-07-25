80c6144960f527d1096ba171aed897d3afeb0cb1
Andrew Platonov <andrew.platonov@protonmail.com>
2023-07-25 19:05:45 +0400

    Fix ALLOW_ENCODED_SLASH bug.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,15 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.util.UrlPathHelper;	
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.demo", "io.jsight"})
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
---
+++ src/main/java/com/example/demo/SpringApplicationConfig.java
@@ -0,0 +1,19 @@
+package example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+@Configuration
+public class SpringApplicationConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+
+        UrlPathHelper helper = new UrlPathHelper();
+        helper.setUrlDecode(false);
+
+        configurer.setUrlPathHelper(helper);
+    }
+}
