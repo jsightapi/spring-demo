fc6cb5989a57598e929ed10fea73182042a44c92
Andrew Platonov <andrew.platonov@protonmail.com>
2023-07-27 14:07:52 +0300

    Added relaxedPathChars and relaxedQueryChars in Tomcat config.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.util.UrlPathHelper;	
+import org.springframework.web.util.UrlPathHelper;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.example.demo", "io.jsight"})
-public class DemoApplication {
+public class DemoApplication implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
 
 	public static void main(String[] args) {
 		System.setProperty("org.apache.tomcat.util.buf.UDecoder.ALLOW_ENCODED_SLASH", "true");
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+            connector.setAttribute("relaxedPathChars", "<>[\\]^`{|}");
+            connector.setAttribute("relaxedQueryChars", "<>[\\]^`{|}");
+        });
+    }
 }
